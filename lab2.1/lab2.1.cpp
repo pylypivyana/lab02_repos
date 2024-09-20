b837e59e220087c7e7adbb99fda5c4dd1019524d
pylypivyana <yana.pylypiv.ik.2024@lpnu.ua>
2024-09-20 19:20:38 +0300

    Commented z2, uncommented z1

--- lab2.1/lab2.1.cpp
+++ lab2.1/lab2.1.cpp
@@ -8,14 +8,14 @@ using namespace std;
 int main()
 {
 	double a; // вхідний параметр
-	//double z1; // результат обчислення 1-го виразу
-	double z2; // результат обчислення 2-го виразу
+	double z1; // результат обчислення 1-го виразу
+	//double z2; // результат обчислення 2-го виразу
 	cout << "a = "; cin >> a;
-	//z1 = ((sin(4*a))/(1+cos(4*a)))*((cos(2*a))/(1+cos(2*a)));
-	z2 = 1/(tan((3.0/2)*4*atan(1.0)-a));
+	z1 = ((sin(4*a))/(1+cos(4*a)))*((cos(2*a))/(1+cos(2*a)));
+	//z2 = 1/(tan((3.0/2)*4*atan(1.0)-a));
 	cout << endl;
-	//cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
+	cout << "z1 = " << z1 << endl;
+	//cout << "z2 = " << z2 << endl;
 	//new cheange made in github
 	cin.get();
 	return 0;
